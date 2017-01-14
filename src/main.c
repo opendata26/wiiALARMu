cb62c0440b81a951bae558fed35212a8bc719368
Open Data <opendata26git@mail.com>
2017-01-14 17:38:01 +0000

    add audio stuff

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
-//WARNING: Flashing text
-
 #include <string.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <math.h>
 #include "dynamic_libs/os_functions.h"
 #include "dynamic_libs/fs_functions.h"
 #include "dynamic_libs/gx2_functions.h"
@@ -18,10 +17,14 @@
 #include "utils/logger.h"
 #include "utils/utils.h"
 #include "common/common.h"
+#include "main.h"
 
-//Custom print define
+//Custom print function
 #define PRINTU(x, y, ...) { snprintf(msg, 80, __VA_ARGS__); OSScreenPutFontEx(0, x, y, msg); OSScreenPutFontEx(1, x, y, msg); }
 
+void axFrameCallback();
+
+voiceData voice1; //voiceData is in main.h
 
 // Entry point
 int Menu_Main(void)
@@ -73,12 +76,47 @@ int Menu_Main(void)
     OSScreenEnableEx(0, 1);
     OSScreenEnableEx(1, 1);
 
+    //Init stuff
     char msg[80];
     uint8_t sel_digit = 1;
     uint8_t alarmTime[2];
     int vpadError = -1;
     int delay = 0;
     VPADData vpad;
+
+    //AX stuff
+
+    //init 48KHz renderer
+	unsigned int params[3] = {1, 0, 0};
+	AXInitWithParams(params);
+	AXRegisterFrameCallback((void*)axFrameCallback); //this callback doesn't really do much
+
+	memset((void*)&voice1, 0, sizeof(voice1));
+	voice1.samples = malloc(SAMPLE_BUFFER_MAX_SIZE); //allocate room for samples
+
+	voice1.stopped = 1;
+
+	voice1.voice = AXAcquireVoice(25, 0, 0); //get a voice, priority 25. Just a random number I picked
+	if (!voice1.voice) {
+		return;
+	}
+	AXVoiceBegin(voice1.voice);
+	AXSetVoiceType(voice1.voice, 0);
+
+	//Set volume?
+	unsigned int vol = 0x80000000;
+	AXSetVoiceVe(voice1.voice, &vol);
+
+	//Device mix? Volume of DRC/TV?
+	unsigned int mix[24];
+	memset(mix, 0, sizeof(mix));
+	mix[0] = vol;
+	mix[4] = vol;
+
+	AXSetVoiceDeviceMix(voice1.voice, 0, 0, mix);
+	AXSetVoiceDeviceMix(voice1.voice, 1, 0, mix);
+
+    AXVoiceEnd(voice1.voice);
 
     while(1)
     {
@@ -302,3 +340,49 @@ int Menu_Main(void)
     return EXIT_SUCCESS;
 }
 
+void playAlarm()
+{
+	voice1.newFrequency = (float)(440 * exp((-9 + 1) * log(2)/12));
+    //clear the modified flag
+    voice1.modified = 0;
+
+    DCFlushRange(&voice1.newFrequency, sizeof(voice1.newFrequency));
+    DCFlushRange(&voice1, sizeof(voice1));
+}
+
+ax_buffer_t voiceBuffer;
+
+void axFrameCallback() {
+	if (voice1.stopRequested) {
+		//Stop the voice
+		AXSetVoiceState(voice1.voice, 0);
+		//Clear the flag
+		voice1.stopRequested = 0;
+		voice1.stopped = 1;
+		DCFlushRange(&voice1, sizeof(voice1));
+	} else if (voice1.modified) {
+		//does this really need to happen in the callback?
+		DCInvalidateRange(&voice1, sizeof(voice1));
+		memset(&voiceBuffer, 0, sizeof(voiceBuffer));
+		voiceBuffer.samples = voice1.samples;
+		voiceBuffer.format = 25; //almost definitely wrong
+		voiceBuffer.loop = 1;
+		voiceBuffer.cur_pos = 0;
+		voiceBuffer.end_pos = voice1.numSamples - 1;
+		voiceBuffer.loop_offset = 0;
+
+		unsigned int ratioBits[4];
+		ratioBits[0] = (unsigned int)(0x00010000 * ((float)48000 / (float)AXGetInputSamplesPerSec()));
+		ratioBits[1] = 0;
+		ratioBits[2] = 0;
+		ratioBits[3] = 0;
+
+		AXSetVoiceOffsets(voice1.voice, &voiceBuffer);
+		AXSetVoiceSrc(voice1.voice, ratioBits);
+		AXSetVoiceSrcType(voice1.voice, 1);
+		AXSetVoiceState(voice1.voice, 1);
+		voice1.modified = 0;
+		voice1.stopped = 0;
+		DCFlushRange(&voice1, sizeof(voice1));
+	}
+}
